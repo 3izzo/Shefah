fab99cb13ae32c30a86ad9fbca905827033f2aa8
Obadah Arabi <obharabi@gmail.com>
2021-02-10 01:01:12 +0300

    preprocessing changes

--- preprocess_videos.py
+++ preprocess_videos.py
@@ -1,15 +1,16 @@
+from cv2 import data
 import skvideo.io
-import errno
-import sys
 import fnmatch
 import os
 import numpy as np
 import dlib
 import cv2
+from datetime import datetime
 
 
-input_videos_dir = ".\\Videos"
-output_videos_dir = ".\\PreprocessedVideos"
+input_videos_dir = ".\\GRID\\videos"
+input_aligns_dir = ".\\GRID\\align"
+output_videos_dir = ".\\GRID PRE"
 
 
 def find_files(directory, pattern):
@@ -36,6 +37,7 @@ def get_frames_mouth(detector, predictor, frame):
 
     dets = detector(frame, 1)
     shape = None
+    i = 0
     for k, d in enumerate(dets):
         shape = predictor(frame, d)
         i = -1
@@ -58,11 +60,12 @@ def get_frames_mouth(detector, predictor, frame):
         normalize_ratio = MOUTH_WIDTH / float(mouth_right - mouth_left)
 
     new_img_shape = (
-        int(frame.shape[1] * normalize_ratio), int(frame.shape[0] * normalize_ratio))
+        int(frame.shape[1] * normalize_ratio),
+        int(frame.shape[0] * normalize_ratio),
+    )
     resized_img = cv2.resize(
-        src=frame,
-        dsize=new_img_shape,
-        interpolation=cv2.INTER_CUBIC)
+        src=frame, dsize=new_img_shape, interpolation=cv2.INTER_CUBIC
+    )
     mouth_centroid_norm = mouth_centroid * normalize_ratio
 
     mouth_l = int(mouth_centroid_norm[0] - MOUTH_WIDTH / 2)
@@ -80,20 +83,82 @@ def make_dir(dir):
         pass
 
 
+def from_file(path):
+    with open(path, "r") as f:
+        lines = f.readlines()
+    align = [
+        (int(y[0]) / 1000, int(y[1]) / 1000, y[2])
+        for y in [x.strip().split(" ") for x in lines]
+    ]
+    align = strip(align, ["sp", "sil"])
+    return get_sentence(align)
+
+
+def strip(align, items):
+    return [sub for sub in align if sub[2] not in items]
+
+
+def get_sentence(align):
+    return " ".join([y[-1] for y in align if y[-1] not in ["sp", "sil"]])
+
+
+name_dic = {}
+for align_path in find_files(input_aligns_dir, "*.align"):
+    actual_name = from_file(align_path)
+
+    align_path = align_path.replace(".align", "")
+    id = align_path.split("\\")[-1]
+    name_dic[id] = actual_name
+
+
+def preproc_speaker(speaker_index):
+    for video_path in find_files(
+        input_videos_dir + "\\s" + str(speaker_index), "*.mpg"
+    ):
+        start_time = datetime.now()
+        original_name = video_path.replace(".mpg", "").split("\\")[-1]
+        storage_dir = (
+            output_videos_dir
+            + "\\s"
+            + str(speaker_index)
+            + "\\"
+            + name_dic[original_name]
+        )
+        make_dir(storage_dir + "\\unmirrored")
+        make_dir(storage_dir + "\\mirrored")
+        frame_index = 0
+        for frame in get_video_frames(video_path):
+            cropped_frame = cv2.cvtColor(
+                get_frames_mouth(face_detector, predictor, frame), cv2.COLOR_BGR2RGB
+            )
+            mirrored = cv2.flip(cropped_frame, 1)
+            cv2.imwrite(
+                "%s\\unmirrored\\frame%d.png" % (storage_dir, frame_index),
+                cropped_frame,
+            )
+            cv2.imwrite(
+                "%s\\mirrored\\frame%d.png" % (storage_dir, frame_index), mirrored
+            )
+            frame_index += 1
+        delta_time = datetime.now() - start_time
+        print(
+            "Done",
+            storage_dir,
+            "in",
+            delta_time.microseconds / 1000000 + delta_time.seconds,
+            "s",
+        )
+
+
+import multiprocessing
+from joblib import Parallel, delayed
+
+num_cores = multiprocessing.cpu_count()
+print(num_cores)
 make_dir(output_videos_dir)
 face_detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(".\\shape_predictor_68_face_landmarks.dat")
-for video_path in find_files(input_videos_dir, "[0-9] [0-9].mp4"):
-    storage_dir = video_path.replace(".mp4", "")
-    storage_dir = storage_dir.replace(input_videos_dir, output_videos_dir)
-    make_dir(storage_dir+"\\unmirrored")
-    make_dir(storage_dir+"\\mirrored")
-    i = 0
-    for frame in get_video_frames(video_path):
 
-        cropped_frame = cv2.cvtColor(get_frames_mouth(
-            face_detector, predictor, frame), cv2.COLOR_BGR2RGB)
-        mirrored = cv2.flip(cropped_frame, 1)
-        cv2.imwrite("%s\\unmirrored\\frame%d.png" % (storage_dir, i), cropped_frame)
-        cv2.imwrite("%s\\mirrored\\frame%d.png" % (storage_dir, i), mirrored)
-        i += 1
+Parallel(n_jobs=num_cores)(
+    delayed(preproc_speaker)(speaker_index) for speaker_index in range(1, 13)
+)
